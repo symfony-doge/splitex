35802ac3089012f1f37de16a29c8078821606d5e
Pavel Petrov <itnelo@gmail.com>
2019-07-13 14:17:08 +0300

    concurrent slice sum calculation example

---
+++ example/concurrent_slice_sum.go
@@ -0,0 +1,41 @@
+// Copyright 2019 Pavel Petrov <itnelo@gmail.com>. All rights reserved.
+// Use of this source code is governed by a MIT license
+// that can be found in the LICENSE file.
+
+package example
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/symfony-doge/event"
+	"github.com/symfony-doge/splitex"
+)
+
+func cssConsumeFunc(e event.Event) {
+	fmt.Printf("An event has been received. Type: %d, Payload: %v\n", e.Type, e.Payload)
+}
+
+// ConcurrentSliceSum is a demo code snippet that represents a splitex use case.
+func ConcurrentSliceSum() {
+	fmt.Println("Calculating sum of slice elements using multiple execution flows...")
+
+	fmt.Println("Starting a listening session...")
+
+	listenerSession := event.MustListen(cssConsumeFunc)
+	defer listenerSession.Close()
+
+	var notifyChannel chan<- event.Event = listenerSession.NotifyChannel()
+
+	fmt.Println("Distributing sum calculation between multiple execution flows...")
+
+	var workerPool = splitex.DefaultWorkerPoolWith(NewExampleSplitter(), NewExampleWorkerFactory())
+	var waitGroup, distributeErr = workerPool.Distribute(data, notifyChannel)
+	if nil != distributeErr {
+		fmt.Println("An error has been occurred during Distribute call:", distributeErr)
+
+		os.Exit(1)
+	}
+
+	waitGroup.Wait()
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/symfony-doge/splitex
 
 go 1.12
+
+require github.com/symfony-doge/event v0.1.2-0.20190713105949-848086003a00
